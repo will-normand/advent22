2dc9c37920f52dc13e312e9ef992cec4a52cd2a4
Will Normand <mail@wnormand.co.uk>
2022-12-15 19:30:05 +0000

    Day 11, part 2
    BigIntegers everywhere :|

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,8 +1,9 @@
+import java.math.BigInteger
+
 class Day11(filename: String) : Advent, InputFileReader {
 
-
     data class Monkey(
-        val items: MutableList<Int>,
+        val items: MutableList<BigInteger>,
         val operation: Operation,
         val test: Int,
         val trueDest: Int,
@@ -11,8 +12,8 @@ class Day11(filename: String) : Advent, InputFileReader {
         var inspections = 0
 
         companion object {
-            val add: Int.(Int) -> Int = { b -> plus(b) }
-            val multiply: Int.(Int) -> Int = { b -> times(b) }
+            val add: BigInteger.(BigInteger) -> BigInteger = { b -> plus(b) }
+            val multiply: BigInteger.(BigInteger) -> BigInteger = { b -> times(b) }
 
             fun parseOperation(input: String): Operation {
                 val (a, op, b) = input.trim().removePrefix("Operation: new = ").split(" ")
@@ -25,16 +26,16 @@ class Day11(filename: String) : Advent, InputFileReader {
                 if (a != "old") throw Exception("Unexpected first operand $a")
 
                 return if (b == "old") { x -> x.opFun(x) }
-                else { x -> x.opFun(b.toInt()) }
+                else { x -> x.opFun(b.toBigInteger()) }
             }
 
             private fun parseIntInput(prefix: String, input: String): Int =
                 input.trim().removePrefix(prefix).toInt()
 
             fun parseDivisor(input: String): Int = parseIntInput("Test: divisible by ", input)
-            fun parseStartingList(input: String): MutableList<Int> =
+            fun parseStartingList(input: String): MutableList<BigInteger> =
                 input.trim().removePrefix("Starting items: ").split(',').map {
-                    it.trim().toInt()
+                    it.trim().toBigInteger()
                 }.toMutableList()
 
             fun parseMonkeyNumber(input: String): Int =
@@ -53,15 +54,22 @@ class Day11(filename: String) : Advent, InputFileReader {
                 )
         }
 
-        fun inspect(): Int {
+        private fun inspect(): BigInteger {
             inspections++
             val item = items.removeFirst()
-            val worry = operation(item)
-            val newWorry = worry / 3
-            return newWorry
+            return operation(item)
         }
 
-        fun catch(item: Int) {
+        fun inspectPart1(): BigInteger {
+            val worry = inspect()
+            return worry / 3.toBigInteger()
+        }
+
+        fun inspectPart2(): BigInteger {
+            return inspect()
+        }
+
+        fun catch(item: BigInteger) {
             items.add(items.size, item)
         }
     }
@@ -73,11 +81,15 @@ class Day11(filename: String) : Advent, InputFileReader {
         return monkeyLines.map { Monkey.parseMonkey(it.take(6)) }
     }
 
-    class Game(val monkeys: List<Monkey>) {
+    enum class Part { Part1, Part2 }
+
+    class Game(val monkeys: List<Monkey>, val part: Part) {
+        private val testProduct = monkeys.map { it.test.toBigInteger() }.reduce { a, b -> a.times(b) }
         fun turn(monkey: Monkey) {
             while (monkey.items.isNotEmpty()) {
-                val newWorry = monkey.inspect()
-                if (newWorry % monkey.test == 0) {
+
+                val newWorry = if (part == Part.Part1) monkey.inspectPart1() else (monkey.inspectPart2() % testProduct)
+                if ((newWorry % monkey.test.toBigInteger()) == BigInteger.ZERO) {
                     monkeys[monkey.trueDest].catch(newWorry)
                 } else {
                     monkeys[monkey.falseDest].catch(newWorry)
@@ -92,21 +104,28 @@ class Day11(filename: String) : Advent, InputFileReader {
         fun printState() {
             monkeys.forEachIndexed { index, monkey -> println("Monkey $index: ${monkey.items}") }
         }
+
+        fun printInspections() {
+            monkeys.forEachIndexed { index, monkey -> println("Monkey $index inspected items ${monkey.inspections} times.") }
+        }
     }
 
     override fun part1(): String {
-        val game = Game(parseMonkeys())
+        val game = Game(parseMonkeys(), Part.Part1)
         repeat(20) { game.round() }
-        return game.monkeys.map { it.inspections }.sorted().takeLast(2).reduce{ a, b -> a * b }.toString()
+        game.monkeys.forEach { println(it.inspections) }
+        return game.monkeys.map { it.inspections }.sorted().takeLast(2).reduce { a, b -> a * b }.toString()
     }
 
     override fun part2(): String {
-        return TODO()
+        val game = Game(parseMonkeys(), Part.Part2)
+        repeat(10000) { game.round() }
+        return game.monkeys.map { it.inspections }.sorted().takeLast(2).map { it.toBigInteger() }
+            .reduce { a, b -> a * b }.toString()
     }
 }
 
-typealias Operation = (Int) -> Int
-typealias Test = (Int) -> Boolean
+typealias Operation = (BigInteger) -> BigInteger
 
 fun main() {
     val advent: Advent = Day11("src/main/resources/input11")
--- src/test/kotlin/Day11Test.kt
+++ src/test/kotlin/Day11Test.kt
@@ -11,23 +11,23 @@ class Day11Test {
         @Test
         fun `parseOperation with multiply`() {
             val op = Day11.Monkey.parseOperation("old * 19")
-            assertEquals(19, op(1))
-            assertEquals(38, op(2))
+            assertEquals(19.toBigInteger(), op(1.toBigInteger()))
+            assertEquals(38.toBigInteger(), op(2.toBigInteger()))
         }
 
         @Test
         fun `parseOperation with add`() {
             val op = Day11.Monkey.parseOperation("old + 12")
-            assertEquals(13, op(1))
-            assertEquals(38, op(26))
+            assertEquals(13.toBigInteger(), op(1.toBigInteger()))
+            assertEquals(38.toBigInteger(), op(26.toBigInteger()))
         }
 
         @Test
         fun `parseOperation with two olds`() {
             val square = Day11.Monkey.parseOperation("old * old")
-            assertEquals(36, square(6))
+            assertEquals(36.toBigInteger(), square(6.toBigInteger()))
             val double = Day11.Monkey.parseOperation("old + old")
-            assertEquals(64, double(32))
+            assertEquals(64.toBigInteger(), double(32.toBigInteger()))
         }
 
         @Test
@@ -37,7 +37,10 @@ class Day11Test {
 
         @Test
         fun parseStartingList() {
-            assertEquals(listOf(65, 77), Day11.Monkey.parseStartingList("  Starting items: 65, 77"))
+            assertEquals(
+                listOf(65, 77).map { it.toBigInteger() },
+                Day11.Monkey.parseStartingList("  Starting items: 65, 77")
+            )
         }
 
         @Test
@@ -52,7 +55,7 @@ class Day11Test {
             """.trimIndent().split('\n')
 
             val monkey = Day11.Monkey.parseMonkey(monkeyInput)
-            assertEquals(listOf(90, 92, 63, 91, 96, 63, 64), monkey.items)
+            assertEquals(listOf(90, 92, 63, 91, 96, 63, 64).map { it.toBigInteger() }, monkey.items)
             assertEquals(13, monkey.test)
             assertEquals(4, monkey.trueDest)
             assertEquals(3, monkey.falseDest)
@@ -67,19 +70,18 @@ class Day11Test {
 
     @Test
     fun `do the first round`() {
-        val game = Day11.Game(day11.parseMonkeys())
-        game.printState()
+        val game = Day11.Game(day11.parseMonkeys(), Day11.Part.Part1)
         game.round()
-        assertEquals(mutableListOf(20, 23, 27, 26), game.monkeys[0].items)
-        assertEquals(mutableListOf(2080, 25, 167, 207, 401, 1046), game.monkeys[1].items)
+        assertEquals(mutableListOf(20, 23, 27, 26).map { it.toBigInteger() }, game.monkeys[0].items)
+        assertEquals(mutableListOf(2080, 25, 167, 207, 401, 1046).map { it.toBigInteger() }, game.monkeys[1].items)
     }
 
     @Test
     fun `do 20 rounds`() {
-        val game = Day11.Game(day11.parseMonkeys())
+        val game = Day11.Game(day11.parseMonkeys(), Day11.Part.Part1)
         repeat(20) { game.round() }
-        assertEquals(mutableListOf(10, 12, 14, 26, 34), game.monkeys[0].items)
-        assertEquals(mutableListOf(245, 93, 53, 199, 115), game.monkeys[1].items)
+        assertEquals(mutableListOf(10, 12, 14, 26, 34).map { it.toBigInteger() }, game.monkeys[0].items)
+        assertEquals(mutableListOf(245, 93, 53, 199, 115).map { it.toBigInteger() }, game.monkeys[1].items)
         assertTrue(game.monkeys[2].items.isEmpty())
         assertTrue(game.monkeys[3].items.isEmpty())
     }
@@ -87,6 +89,20 @@ class Day11Test {
     @Test
     fun part1() {
         val result = day11.part1()
+
         assertEquals("10605", result)
+    }
+
+    @Test
+    fun `do the first round in part 2`() {
+        val game = Day11.Game(day11.parseMonkeys(), Day11.Part.Part2)
+        repeat(20) { game.round() }
+        game.printInspections()
+    }
+
+    @Test
+    fun part2() {
+        val result = day11.part2()
+        assertEquals("2713310158", result)
     }
 }
